a2147a1172c0b0e9cab7208b51f2c16bffd285f9
hawkchen <hawkchen@potix.com>
2020-11-11 09:39:09 +0800

    fix the desktop is null problem

--- nodejs-app/public/keikai-controller.js
+++ nodejs-app/public/keikai-controller.js
@@ -1,12 +1,20 @@
 zEmbedded.load('embeddedZK', 'http://localhost:8080/zkembedded-app/keikai.zul')
     .then(function(result) {
-        var spreadsheet = result.widget;
+
+        //jq is a custom jQuery $ in zk
         jq('#a_load').on('click', () => {
-            spreadsheet.fire("onImport", {file: 'demo_sample.xlsx'}, {toServer:true})
+            //need to get spreadsheet widget each time since spreadsheet's desktop might be null in some cases
+            zk.Widget.$('$spreadsheet').fire("onImport", {file: 'demo_sample.xlsx'}, {toServer:true});
         });
-        jq('#a_toggleToolbar').on('click', () => spreadsheet.fire("onToggleVisibility", {ui: "toolbar"}, {toServer:true}) );
-        jq('#a_toggleSheetbar').on('click', () => spreadsheet.fire("onToggleVisibility", {ui: "sheetbar"}, {toServer:true}));
-        jq('#a_toggleFormulabar').on('click', () => spreadsheet.fire("onToggleVisibility", {ui: "formulabar"}, {toServer:true}));
+        jq('#a_toggleToolbar').on('click', () => {
+            zk.Widget.$('$spreadsheet').fire("onToggleVisibility", {ui: "toolbar"}, {toServer:true});
+        });
+        jq('#a_toggleSheetbar').on('click', () => {
+            zk.Widget.$('$spreadsheet').fire("onToggleVisibility", {ui: "sheetbar"}, {toServer:true});
+        });
+        jq('#a_toggleFormulabar').on('click', () => {
+            zk.Widget.$('$spreadsheet').fire("onToggleVisibility", {ui: "formulabar"}, {toServer:true});
+        });
     })
     .catch(function(reason) {
         console.error("error", this, arguments);
